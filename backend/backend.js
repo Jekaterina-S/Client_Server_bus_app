5c95d77852f0f968343f3f958d55991c64c52e68
Jekaterina-S <snotkina@ut.ee>
2025-02-02 21:02:02 +0000

    Update backend.js

--- backend/backend.js
+++ backend/backend.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const { connectToDatabase } = require('./dbconnection');
 const sql = require('mssql');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
